fe583445a3da9da5823c7b92e8aa7771bede40c3
Manu Cheremeh <manucheremeh1995@gmail.com>
2021-10-10 16:48:22 +0200

    correction commmentaire

--- tri_a_bulle.py
+++ tri_a_bulle.py
@@ -6,12 +6,12 @@ def tri_a_bulle(table):
     """
     fonction pour tri a bulle
     """
-    stop = len(table) - 1 # variable pour determiner l'indice qui dbute le tri
+    stop = len(table) - 1 # variable pour determiner l'indice auquel le tri termine
 
-    for i in range(stop):
+    for i in range(stop): 
         for j in range(stop - i):
-            if table[j] > table[j + 1]:
-                table[j], table[j + 1] = table[j + 1], table[j]
+            if table[j] > table[j + 1]: # si la valeur de gauche est plus grande que la valeur suivante de droite
+                table[j], table[j + 1] = table[j + 1], table[j] # intervertir les valeurs
 
     return table
 
