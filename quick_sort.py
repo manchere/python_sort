f00da99990dccd5f6155cfb2a35116f5586bcb08
Manu Cheremeh <manucheremeh1995@gmail.com>
2021-10-19 02:08:46 +0200

    working on quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -1,20 +1,23 @@
-def partitionneur(pivot, table = []):
-    small_indice = 0
-    indice = 0 
-    pivot, table[0] = table[0], pivot
+def pivInd(table):
+    return int(round(len(table) * 0.5))
 
-    for indice in range(len(table) - 1):
-        if table[indice] < pivot:
+def partitionneur(table):
+    small_indice = 0
+    table[0], table[pivInd(table)] = table[pivInd(table)], table[0]
+
+    for indice in range(1, len(table)):
+        if table[indice] < table[pivInd(table)]:
             small_indice += 1
             table[small_indice], table[indice] = table[indice], table[small_indice]
-    
-    pivot, table[small_indice] = table[small_indice], pivot
+
+    table[pivInd(table)], table[small_indice] = table[small_indice], table[pivInd(table)]
 
     return table
-
 
 t = [23,4,63,5,6,5,2,10]
 
 
+# def rapide(start, end, partionneur):
 
-print(partitionneur(t[-1], t))
+print(partitionneur(t))
+
